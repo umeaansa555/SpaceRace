b61cc006c47dfab4f35987f4509888ef9a548b78
P4 <umeaansa555@ed.amdsb.ca>
2022-01-18 12:49:34 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceRace
+{
+    public partial class Form1 : Form
+    {
+        Rectangle player1 = new Rectangle(10, 170, 10, 60);
+        Rectangle player2 = new Rectangle(10, 170, 10, 60);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void engine_Tick(object sender, EventArgs e)
+        {
+
+
+            Refresh();
+        }
+    }
+}
